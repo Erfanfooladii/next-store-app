0283a7c2ec47a7d6fc4166a52719734f83212a79
Erfanfooaldii <erfanfooladi2002@gmail.com>
2025-03-09 19:05:14 +0330

    feat: create home page and components

---
+++ src/app/components/ListProducts/index.tsx
@@ -0,0 +1,35 @@
+import { Data, Product } from "@/app/Interfaces/products";
+import Link from "next/link";
+
+const ListProducts = ({ data }: { data: Data }) => {
+  return (
+    <ul className="flex flex-col justify-between sm:flex-wrap sm:flex-row gap-5">
+      {data.products.map((product: Product) => (
+        <li
+          key={product._id}
+          className="flex gap-4 w-full sm:w-64 flex-col bg-slate-300 p-2 shadow-lg rounded-md"
+        >
+          <Link href={product._id}>
+            <div className="w-full h-52">
+              <img
+                src={product.images[0]}
+                alt="image product"
+                className="w-full h-full"
+              />
+            </div>
+            <div className="flex flex-col">
+              <h3 className="text-lg">{product.name}</h3>
+              <p>
+                price: <span>{product.price}</span>$
+              </p>
+              <button className="p-2 bg-orange-400 hover:bg-orange-300 text-white rounded-md">
+                Add to cart
+              </button>
+            </div>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+export default ListProducts;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,37 @@
+"use client";
+import { getData } from "@/utils/getApi";
+import { useQuery } from "@tanstack/react-query";
+import ListProducts from "./components/ListProducts";
+import LivePrice from "./components/LivePrece";
+import { Data } from "./Interfaces/products";
+import Pagination from "./components/Pagination";
 
 export default function Home() {
+  const fetchData = async (): Promise<Data | undefined> => {
+    try {
+      const response: Data | undefined = await getData();
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      throw error;
+    }
+  };
+
+  const { isLoading, data, error } = useQuery<Data | undefined, Error>({
+    queryKey: ["data"],
+    queryFn: fetchData,
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+  if (!data || !data.products) return <div>No data found</div>;
+
   return (
-    <div className="">
-      Home page
+    <div className="px-2 py-3">
+      <LivePrice />
+      <ListProducts data={data} />
+      <Pagination data={data} />
     </div>
   );
 }
