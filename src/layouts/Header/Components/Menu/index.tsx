341b715111a6a0e777f0911fee0d3e7dd4503b43
Erfanfooaldii <erfanfooladi2002@gmail.com>
2025-03-13 17:59:18 +0330

    feat: create category

---
+++ src/layouts/Header/Components/Menu/index.tsx
@@ -0,0 +1,11 @@
+import Category from "../Categorey";
+
+const Menu = () => {
+  return (
+    <div className="w-64 p-2 fixed left-0 bg-orange-400">
+      <h1>Store</h1>
+      <Category />
+    </div>
+  );
+};
+export default Menu;
