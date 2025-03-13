6ab8c6cf87c3f4ffe3644f1fb87b49aaa99fd248
Erfanfooaldii <erfanfooladi2002@gmail.com>
2025-03-13 17:58:12 +0330

    docs: change file

---
+++ src/Interfaces/products.ts
@@ -0,0 +1,25 @@
+interface Category {
+  _id: string;
+  name: string;
+}
+export interface Product {
+  _id: string;
+  images: Array<string>;
+  name: string;
+  price: string;
+  rating: string;
+  ratingCount: string;
+  stock: string;
+  category: Category;
+  description: string;
+}
+export interface Pagination {
+  currentPage: number;
+  itemsPerPage: number;
+  totalItems: number;
+  totalPages: number;
+}
+export interface Data {
+  pagination: Pagination;
+  products: Product[];
+}
