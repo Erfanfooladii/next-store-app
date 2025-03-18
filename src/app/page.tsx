e52f909a771adc3f73c068f2f362f2f4a24f99a8
Erfanfooaldii <erfanfooladi2002@gmail.com>
2025-03-18 22:37:12 +0330

    revert: remove line

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 import ListProducts from "./Info/_Components/ListProducts";
-import { useContext, useState } from "react";
 import Aside from "./Info/_Components/Aside";
 import Pagination from "./Info/_Components/Pagination";
 
 export default function Home() {
   return (
     <div className="px-2 py-3">
-      {/* <LivePrice /> */}
       <div className="flex gap-3">
         <ListProducts />
         <Aside />
