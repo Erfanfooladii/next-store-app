c9a2f2b19a40812910018c9f8ed924ab179f43a9
Erfanfooaldii <erfanfooladi2002@gmail.com>
2025-03-18 22:24:54 +0330

    docs: create provider for responsive size mobile

---
+++ src/Providers/ResponsiveSize/index.tsx
@@ -0,0 +1,26 @@
+import useWindowDimensions from "@/utils/media.hook";
+import { ReactNode, useEffect, useState } from "react";
+
+const ProviderMobileSize = ({
+  children,
+  isReverse = false,
+}: {
+  children: ReactNode;
+  isReverse: boolean;
+}) => {
+  const sizeScreen = useWindowDimensions();
+  const [isRender, setIsRender] = useState<boolean>(false);
+  useEffect(() => {
+    if (sizeScreen.width !== null && sizeScreen.width > 500) {
+      setIsRender(true);
+    } else {
+      setIsRender(false);
+    }
+  }, [sizeScreen.width]);
+  if (isReverse) {
+    return isRender ? null : children;
+  } else {
+    return isRender ? children : null;
+  }
+};
+export { ProviderMobileSize };
