3ccbca222835666fd279298afde9d94b6fc35c9c
Erfanfooaldii <erfanfooladi2002@gmail.com>
2025-03-19 09:52:26 +0330

    docs: create state for responsive mobile

--- src/layouts/Header/Components/BottomContnet/Components/ContactDetails/index.tsx
+++ src/layouts/Header/Components/BottomContnet/Components/ContactDetails/index.tsx
@@ -1,14 +1,28 @@
+import useWindowDimensions from "@/utils/media.hook";
 import { dataDetails } from "./data";
 const ContactDetails = () => {
-  return (
-    <ul className="flex gap-3">
-      {dataDetails.map((item, index) => (
-        <li key={index} className="flex gap-1">
-          <span>{item.icon}</span>
-          <span>{item.name}</span>
-        </li>
-      ))}
-    </ul>
-  );
+  const { width } = useWindowDimensions();
+  if (width !== null && width > 500) {
+    return (
+      <ul className="flex gap-3">
+        {dataDetails.map((item, index) => (
+          <li key={index} className="flex gap-1">
+            <span>{item.icon}</span>
+            <span>{item.name}</span>
+          </li>
+        ))}
+      </ul>
+    );
+  } else {
+    return (
+      <select className="max-w-24 p-2 rounded-md">
+        {dataDetails.map((item, index) => (
+          <option key={index} className="flex">
+            <span>{item.name}</span>
+          </option>
+        ))}
+      </select>
+    );
+  }
 };
 export default ContactDetails;
