82bb3f7c19edbf77526280a9e009f86e7bdc7317
Erfanfooaldii <erfanfooladi2002@gmail.com>
2025-03-23 13:26:58 +0330

    styles:change style for aside component

--- src/app/Info/_Components/Aside/index.tsx
+++ src/app/Info/_Components/Aside/index.tsx
@@ -1,12 +1,16 @@
 import Category from "@/layouts/Header/Components/Categorey";
 import { ProviderMobileSize } from "@/Providers/ResponsiveSize";
+import TopSellins from "../TopSellins";
 
 const Aside = () => {
   return (
     <ProviderMobileSize isReverse={false}>
-      <aside className="p-2 col-span-2 min-w-48 row-span-3 rounded-md bg-orange-400">
+      <aside className="p-2  min-w-52 flex flex-col gap-1 rounded-md bg-orange-400">
         <div className="border-orange-100 p-1 border-2 rounded">
           <Category />
+        </div>
+        <div className="border-orange-100 p-1 border-2 rounded">
+          <TopSellins />
         </div>
       </aside>
     </ProviderMobileSize>
