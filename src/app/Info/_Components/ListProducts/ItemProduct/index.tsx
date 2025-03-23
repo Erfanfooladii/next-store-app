c1dfec248ed274062aeee4c405ed6e8ce033084b
Erfanfooaldii <erfanfooladi2002@gmail.com>
2025-03-23 15:42:35 +0330

    refactor: change code for create item product

---
+++ src/app/Info/_Components/ListProducts/ItemProduct/index.tsx
@@ -0,0 +1,31 @@
+import { Product } from "@/Interfaces/products";
+import Link from "next/link";
+
+const ItemProduct = ({ product }: { product: Product }) => {
+  return (
+    <li
+      key={product._id}
+      className=" bg-slate-300 w-full sm:w-72 p-2 shadow-lg rounded-md"
+    >
+      <Link href={product._id}>
+        <div className="w-full h-52">
+          <img
+            src={product.images[0]}
+            alt="image product"
+            className="w-full h-full"
+          />
+        </div>
+        <div className="grid grid-cols-1 grid-rows-3">
+          <h3 className="text-lgd">{product.name}</h3>
+          <h4 className="text-xl md:text-lg">
+            category: {product.category.name}
+          </h4>
+          <button className="p-2 bg-orange-400 hover:bg-orange-300 text-white rounded-md">
+            Add to cart
+          </button>
+        </div>
+      </Link>
+    </li>
+  );
+};
+export default ItemProduct;
--- src/app/Info/_Components/ListProducts/index.tsx
+++ src/app/Info/_Components/ListProducts/index.tsx
@@ -1,13 +1,14 @@
 import { useGetProduct } from "@/app/api/products/hook";
 import ShowItem from "@/Components/Show";
-import { Data, Product } from "@/Interfaces/products";
+import { Product } from "@/Interfaces/products";
 import useCategory from "@/stores/categoryStore";
 import useData from "@/stores/dataStore";
 import usePage from "@/stores/pageStore";
 import useSearch from "@/stores/searchStore";
 import useShowItem from "@/stores/showStore";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import ItemProduct from "./ItemProduct";
 
 const ListProducts = () => {
   const { search } = useSearch();
@@ -38,29 +39,7 @@ const ListProducts = () => {
       <ShowItem />
       <ul className="flex w-full justify-center flex-wrap gap-4 flex-row">
         {data.products.map((product: Product) => (
-          <li
-            key={product._id}
-            className=" bg-slate-300 w-full sm:w-72 p-2 shadow-lg rounded-md"
-          >
-            <Link href={product._id}>
-              <div className="w-full h-52">
-                <img
-                  src={product.images[0]}
-                  alt="image product"
-                  className="w-full h-full"
-                />
-              </div>
-              <div className="grid grid-cols-1 grid-rows-3">
-                <h3 className="text-lgd">{product.name}</h3>
-                <h4 className="text-xl md:text-lg">
-                  category: {product.category.name}
-                </h4>
-                <button className="p-2 bg-orange-400 hover:bg-orange-300 text-white rounded-md">
-                  Add to cart
-                </button>
-              </div>
-            </Link>
-          </li>
+          <ItemProduct product={product} />
         ))}
       </ul>
     </div>
