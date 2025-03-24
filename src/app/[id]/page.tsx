72c1ba8e989e6b4bcd9afc60820d56374cc6a1c2
Erfanfooaldii <erfanfooladi2002@gmail.com>
2025-03-25 00:22:33 +0330

    docs:change product page for async component

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,8 +1,8 @@
 import ListDetails from "./Info/_Components/ListDetails";
-const Product = ({ params }: { params: { id: string } }) => {
-  const { id } = params;
+const Product = async ({ params }: { params: { id: string } }) => {
+  const { id } = await params;
   return (
-    <div className="bg-orange-400 rounded-md shadow-lg p-2">
+    <div className="bg-orange-400 min-h-[550px] rounded-md shadow-lg p-2">
       <ListDetails id={id} />
     </div>
   );
