33d2394f4fd906c4b8a77892c9baf6db772452e2
Erfanfooaldii <erfanfooladi2002@gmail.com>
2025-03-27 01:02:09 +0330

    style

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -2,7 +2,7 @@ import ListDetails from "./Info/_Components/ListDetails";
 const Product = async ({ params }: { params: { id: string } }) => {
   const { id } = await params;
   return (
-    <div className="bg-orange-400 min-h-[550px] rounded-md shadow-lg p-2">
+    <div className="py-2 min-h-[650px] md:min-h-[750px] md:flex md:items-center md:gap-2">
       <ListDetails id={id} />
     </div>
   );
