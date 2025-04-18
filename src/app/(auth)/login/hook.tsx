51b27148e0bb882c3115a86405d29f66f7647803
Erfanfooaldii <erfanfooladi2002@gmail.com>
2025-04-18 22:58:51 +0330

    pref: create hook file for login page

---
+++ src/app/(auth)/login/hook.tsx
@@ -0,0 +1,36 @@
+import { LoginType } from "@/app/(auth)/types";
+import { useAuthStore } from "@/stores/authStore";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export const useLoginHandeler = () => {
+  const { push } = useRouter();
+  const [userAuth, setUserAuth] = useState<LoginType>({
+    password: "",
+    email: "",
+  });
+  const { loginUserAuth, currentUser } = useAuthStore();
+  console.log(currentUser);
+
+  const loginHandeler = (e: React.FormEvent) => {
+    e.preventDefault();
+    loginUserAuth(userAuth);
+    setUserAuth({
+      email: "",
+      password: "",
+    });
+    push("/");
+  };
+  const inputHandeler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUserAuth((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  return {
+    inputHandeler,
+    loginHandeler,
+    userAuth,
+  };
+};
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,23 +1,37 @@
+"use client";
 import Link from "next/link";
+import React from "react";
+import { useLoginHandeler } from "./hook";
 
 const Login = () => {
+  const { loginHandeler, inputHandeler, userAuth } = useLoginHandeler();
   return (
     <div className="min-h-[750px] flex items-center justify-center min-w-32">
-      <form className="flex border border-orange-900 p-4 rounded-lg gap-2 flex-col items-center">
+      <form
+        onSubmit={loginHandeler}
+        className="flex border border-orange-900 p-4 rounded-lg gap-2 flex-col items-center"
+      >
         <h1 className="text-2xl font-semibold">Login Form</h1>
         <div className="flex justify-between items-center gap-2">
-          <label htmlFor="name">Your email:</label>
+          <label htmlFor="email">Your email:</label>
           <input
             type="text"
-            id="name"
+            id="email"
+            name="email"
+            onChange={inputHandeler}
+            value={userAuth.email}
             className="p-1 shadow focus:outline-none rounded-md"
+            required
           />
         </div>
         <div className="flex justify-between items-center gap-2">
-          <label htmlFor="name">Password:</label>
+          <label htmlFor="password">Password:</label>
           <input
             type="text"
-            id="name"
+            id="password"
+            name="password"
+            onChange={inputHandeler}
+            value={userAuth.password}
             className="p-1 shadow focus:outline-none rounded-md"
           />
         </div>
