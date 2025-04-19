777f84716b532428a9aa6c059a7c453992687f01
Erfanfooaldii <erfanfooladi2002@gmail.com>
2025-04-19 18:55:57 +0330

    style: change text button login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -36,7 +36,7 @@ const Login = () => {
           />
         </div>
         <button className="bg-orange-400 px-10 py-2 shadow hover:bg-orange-300 rounded-full">
-          Register
+          Login
         </button>
         <div>
           <p>
