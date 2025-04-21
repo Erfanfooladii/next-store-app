9ec8e6e471b6572fc89a51db620789b0b8b3f802
Erfanfooaldii <erfanfooladi2002@gmail.com>
2025-04-21 17:38:43 +0330

    docs: change components in register page

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,68 +1,41 @@
 "use client";
-import Link from "next/link";
 import React from "react";
 import { useRegisterUser } from "./hook";
+import FormAuth from "../_components/FormAuth";
+import InputAuth from "../_components/InputAuth";
+import ButtonAuth from "../_components/ButtonAuth";
+import BottomAuth from "../_components/BottomAuth";
 const Register = () => {
   const { formHandler, registerFromUser, inputHandler } = useRegisterUser();
   return (
     <div className="min-h-[750px] flex items-center justify-center min-w-32">
-      <form
-        onSubmit={formHandler}
-        className="flex border border-orange-900 p-4 rounded-lg gap-2 flex-col items-center"
-      >
-        <h1 className="text-2xl font-semibold">Register Form</h1>
-        <div className="flex justify-between items-center gap-2">
-          <label htmlFor="name">Your name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={registerFromUser.name}
-            onChange={inputHandler}
-            className="p-1 shadow focus:outline-none rounded-md"
-            required
-          />
-        </div>
-        <div className="flex justify-between items-center gap-2">
-          <label htmlFor="email">Your email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            onChange={inputHandler}
-            value={registerFromUser.email}
-            className="p-1 shadow focus:outline-none rounded-md"
-            required
-          />
-        </div>
-        <div className="flex justify-between items-center gap-2">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            onChange={inputHandler}
-            value={registerFromUser.password}
-            className="p-1 shadow focus:outline-none rounded-md"
-            required
-            minLength={6}
-          />
-        </div>
-        <button
-          type="submit"
-          className="bg-orange-400 px-10 py-2 shadow hover:bg-orange-300 rounded-full"
-        >
-          Register
-        </button>
-        <div>
-          <p>
-            Already have an account?{" "}
-            <Link className="text-gray-600 hover:underline" href="/login">
-              Login in
-            </Link>
-          </p>
-        </div>
-      </form>
+      <FormAuth title="Register Form" formHandler={formHandler}>
+        <InputAuth
+          value={registerFromUser.name}
+          inputHandler={inputHandler}
+          nameInput="name"
+          label="Your name"
+        />
+        <InputAuth
+          value={registerFromUser.email}
+          inputHandler={inputHandler}
+          nameInput="email"
+          label="Your email"
+        />
+        <InputAuth
+          value={registerFromUser.password}
+          inputHandler={inputHandler}
+          nameInput="password"
+          label="Password"
+          type="password"
+        />
+        <ButtonAuth type="submit">Register</ButtonAuth>
+        <BottomAuth
+          title="Already have an account?"
+          contentLink="Login"
+          href="/login"
+        />
+      </FormAuth>
     </div>
   );
 };
